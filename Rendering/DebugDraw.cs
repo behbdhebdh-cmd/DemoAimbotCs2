d2073ac2bf0e5088aee115e777a74cde873e2066
behbdhebdh-cmd <behbdhebdh@gmail.com>
2025-12-14 11:23:49 +0100

    Update DebugDraw.cs

--- Rendering/DebugDraw.cs
+++ Rendering/DebugDraw.cs
@@ -1,1 +1,11 @@
+using System.Drawing;
+
+namespace DemoAimbotTool.Rendering
+{
+    public static class DebugDraw
+    {
+        public static System.Action<float, float, float, Color> DrawCircle;
+        public static System.Action<float, float, float, float, Color> DrawLine;
+    }
+}
 
