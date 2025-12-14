199dbc1c62ea2efb7b35166d1651f110ff9e2591
behbdhebdh-cmd <behbdhebdh@gmail.com>
2025-12-14 11:22:49 +0100

    Update MathHelpers.cs

--- math/MathHelpers.cs
+++ math/MathHelpers.cs
@@ -1,1 +1,16 @@
+namespace DemoAimbotTool.Math
+{
+    public static class MathHelpers
+    {
+        public static Angle NormalizeAngles(Angle a)
+        {
+            while (a.Yaw > 180) a.Yaw -= 360;
+            while (a.Yaw < -180) a.Yaw += 360;
+            if (a.Pitch > 89) a.Pitch = 89;
+            if (a.Pitch < -89) a.Pitch = -89;
+            a.Roll = 0;
+            return a;
+        }
+    }
+}
 
