8e25fa62651427ac4899aa150b6297313df8b5e0
behbdhebdh-cmd <behbdhebdh@gmail.com>
2025-12-14 11:24:03 +0100

    Update DxOverlay.cs

--- Rendering/DirectX/DxOverlay.cs
+++ Rendering/DirectX/DxOverlay.cs
@@ -1,1 +1,31 @@
+using System.Windows.Forms;
+
+namespace DemoAimbotTool.Rendering.DirectX
+{
+    public class DxOverlay : Form
+    {
+        public DxRenderer Renderer { get; }
+
+        public DxOverlay()
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            TopMost = true;
+            ShowInTaskbar = false;
+            BackColor = System.Drawing.Color.Black;
+            TransparencyKey = System.Drawing.Color.Black;
+            Bounds = Screen.PrimaryScreen.Bounds;
+            Renderer = new DxRenderer(this);
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                var cp = base.CreateParams;
+                cp.ExStyle |= 0x80000 | 0x20;
+                return cp;
+            }
+        }
+    }
+}
 
