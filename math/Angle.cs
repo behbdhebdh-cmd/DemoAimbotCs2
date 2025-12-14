f1c7def3c2d22a2e7a1c9f36f183e353ceef26ab
behbdhebdh-cmd <behbdhebdh@gmail.com>
2025-12-14 11:22:38 +0100

    Update Angle.cs

--- math/Angle.cs
+++ math/Angle.cs
@@ -1,1 +1,12 @@
+namespace DemoAimbotTool.Math
+{
+    public struct Angle
+    {
+        public float Pitch, Yaw, Roll;
+        public Angle(float p, float y, float r = 0)
+        {
+            Pitch = p; Yaw = y; Roll = r;
+        }
+    }
+}
 
