4e3c44fd6aa48990367313f5aabcac921a49680c
behbdhebdh-cmd <behbdhebdh@gmail.com>
2025-12-14 11:22:20 +0100

    Update Vector3.cs

--- math/Vector3.cs
+++ math/Vector3.cs
@@ -1,1 +1,17 @@
+using System;
+
+namespace DemoAimbotTool.Math
+{
+    public struct Vector3
+    {
+        public float X, Y, Z;
+        public Vector3(float x, float y, float z) { X = x; Y = y; Z = z; }
+
+        public static Vector3 operator -(Vector3 a, Vector3 b)
+            => new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+
+        public float Length()
+            => MathF.Sqrt(X * X + Y * Y + Z * Z);
+    }
+}
 
