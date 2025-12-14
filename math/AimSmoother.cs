261d1c83352aeb0e1b470fcbb2992ccb12a98c9b
behbdhebdh-cmd <behbdhebdh@gmail.com>
2025-12-14 11:23:14 +0100

    Update AimSmoother.cs

--- math/AimSmoother.cs
+++ math/AimSmoother.cs
@@ -1,1 +1,29 @@
+using System;
+
+namespace DemoAimbotTool.Math
+{
+    public class AimSmoother
+    {
+        public float Smoothing = 12f;
+
+        public Angle Update(Angle cur, Vector3 cam, Vector3 tgt, float dt)
+        {
+            Angle dst = CameraMath.CalcAngle(cam, tgt);
+            float t = 1f - MathF.Exp(-Smoothing * dt);
+
+            cur.Pitch += Delta(cur.Pitch, dst.Pitch) * t;
+            cur.Yaw += Delta(cur.Yaw, dst.Yaw) * t;
+
+            return MathHelpers.NormalizeAngles(cur);
+        }
+
+        private float Delta(float a, float b)
+        {
+            float d = b - a;
+            while (d > 180) d -= 360;
+            while (d < -180) d += 360;
+            return d;
+        }
+    }
+}
 
